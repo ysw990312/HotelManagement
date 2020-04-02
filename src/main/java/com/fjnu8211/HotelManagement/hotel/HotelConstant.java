15ae15099f7178a728978d16f4700fc35362be67
ysw990312 <729471949@qq.com>
2020-04-03 03:33:09 +0800

    添加常量类

---
+++ src/main/java/com/fjnu8211/HotelManagement/hotel/HotelConstant.java
@@ -0,0 +1,9 @@
+package com.fjnu8211.HotelManagement.hotel;
+
+public class HotelConstant {
+
+	public int HOTEL_TOTAL_FLOOR = 10;
+	public int HOTEL_ROOMS_EVERYFLOOR = 12;
+	public String EMPTY = "EMPTY";
+	
+}
