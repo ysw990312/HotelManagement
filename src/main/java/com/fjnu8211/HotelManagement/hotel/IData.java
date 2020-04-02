efc8489e830b1538cb04225847bbfb4ad2852058
ysw990312 <729471949@qq.com>
2020-04-03 02:54:24 +0800

    添加一个IData接口

---
+++ src/main/java/com/fjnu8211/HotelManagement/hotel/IData.java
@@ -0,0 +1,12 @@
+package com.fjnu8211.HotelManagement.hotel;
+
+/*
+ * IData:接口的定义
+ */
+public interface IData {
+
+	public void iniRooms();
+	public String in_Out_Room(int roomNo, String name);
+	public String getStation(int roomNO);
+	
+}
